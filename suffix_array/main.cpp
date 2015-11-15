ed97bfc5ac8c6ffeafc849865f84a3cd895d9cd8
Daniil Dmitriev <ocksumoron@yandex.ru>
2015-11-16 00:23:24 +0300

    Initial Commit

---
+++ suffix_array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  suffix_array
+//
+//  Created by Даня on 16.11.15.
+//  Copyright (c) 2015 mipt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
