ac70d880bf50649fb5ed44d969af3f2c207b4b5a
Daniil Dmitriev <ocksumoron@yandex.ru>
2015-12-10 00:26:47 +0300

    best code style

--- suffix_array/SuffixArrayBuilder.cpp
+++ suffix_array/SuffixArrayBuilder.cpp
@@ -29,7 +29,8 @@ vector<int> SuffixArrayBuilder::radixPass(vector<int> &toSort, vector<int> &keys
 void SuffixArrayBuilder::buildSuffixArray() {
     bool extraEnding = false;
     int n = str.size();
-    int n0 = (n + 2) / 3, n2 = n / 3;
+    int suffsMod0 = (n + 2) / 3, suffsMod2 = n / 3;
+    int suffsMod02 = suffsMod0 + suffsMod2;
     
     str.push_back(0);
     str.push_back(0);
@@ -46,7 +47,7 @@ void SuffixArrayBuilder::buildSuffixArray() {
         }
     }
     
-    vector<int> radixSort(n0 + n2), sort12(n0 + n2), unmodifiedSA12(n0 + n2), SA12(n0 + n2), SA0(n0), inversedSA12(n);
+    vector<int> radixSort(suffsMod02), sort12(suffsMod02), unmodifiedSA12(suffsMod02), SA12(suffsMod02), SA0(suffsMod0), inversedSA12(n);
     
     radixSort = radixPass(triples, str, 2);
     radixSort = radixPass(radixSort, str, 1);
@@ -56,7 +57,7 @@ void SuffixArrayBuilder::buildSuffixArray() {
     tuple<int, int, int> lastTriple, currentTriple;
     lastTriple = {-1, -1, -1};
     
-    for (int i = 0; i < n0 + n2; ++i) {
+    for (int i = 0; i < suffsMod02; ++i) {
         currentTriple = {str[radixSort[i]], str[radixSort[i] + 1], str[radixSort[i] + 2]};
         if (currentTriple != lastTriple) {
             lastTriple = currentTriple;
@@ -65,29 +66,29 @@ void SuffixArrayBuilder::buildSuffixArray() {
         if (radixSort[i] % 3 == 1) {
             sort12[radixSort[i] / 3] = currentTriplesSize;
         } else {
-            sort12[radixSort[i] / 3 + n0] = currentTriplesSize;
+            sort12[radixSort[i] / 3 + suffsMod0] = currentTriplesSize;
         }
     }
     
-    if (currentTriplesSize < n0 + n2) {
+    if (currentTriplesSize < suffsMod02) {
         SuffixArrayBuilder nextBuilder = SuffixArrayBuilder(sort12, currentTriplesSize + 1);
         radixSort = nextBuilder.getSuffixArray();
     } else {
-        for (int i = 0; i < n0 + n2; ++i) {
+        for (int i = 0; i < suffsMod02; ++i) {
             radixSort[sort12[i] - 1] = i;
         }
     }
     
-    for (int i = 0; i < n0 + n2; ++i) {
-        if (radixSort[i] < n0) {
+    for (int i = 0; i < suffsMod02; ++i) {
+        if (radixSort[i] < suffsMod0) {
             SA12[i] = 1 + 3 * radixSort[i];
         } else {
-            SA12[i] = 2 + 3 * (radixSort[i] - n0);
+            SA12[i] = 2 + 3 * (radixSort[i] - suffsMod0);
         }
     }
     
     int j = 0;
-    for (int i = 0; i < n0 + n2; ++i) {
+    for (int i = 0; i < suffsMod02; ++i) {
         if ((SA12[i] % 3) == 1) {
             SA0[j++] = SA12[i] - 1;
         }
@@ -97,7 +98,7 @@ void SuffixArrayBuilder::buildSuffixArray() {
     
     int indexSA12 = 0, indexSA0 = 0;
     
-    for (int i = 0; i < n0 + n2; ++i) {
+    for (int i = 0; i < suffsMod02; ++i) {
         inversedSA12[SA12[i]] = i;
     }
     
@@ -105,7 +106,7 @@ void SuffixArrayBuilder::buildSuffixArray() {
     if (extraEnding) {
         indexSA12++;
     }
-    while (indexSA12 < n0 + n2 && indexSA0 < n0) {
+    while (indexSA12 < suffsMod02 && indexSA0 < suffsMod0) {
         bool toAddSA0Suffix;
         if ((SA12[indexSA12] % 3) == 1) {
             pair <int, int> SA12Suffix, SA0Suffix;
@@ -125,12 +126,12 @@ void SuffixArrayBuilder::buildSuffixArray() {
         }
     }
 
-    if (indexSA12 == n0 + n2) {
-        while (indexSA0 < n0) {
+    if (indexSA12 == suffsMod02) {
+        while (indexSA0 < suffsMod0) {
             suffixArray[indexSA++] = SA0[indexSA0++];
         }
     } else {
-        while (indexSA12 < n0 + n2) {
+        while (indexSA12 < suffsMod02) {
             suffixArray[indexSA++] = SA12[indexSA12++];
         }
     }
@@ -167,7 +168,6 @@ void SuffixArrayBuilder::buildLCP() {
 
 SuffixArrayBuilder::SuffixArrayBuilder() {}
 SuffixArrayBuilder::~SuffixArrayBuilder() {}
-
 
 SuffixArrayBuilder::SuffixArrayBuilder(const string &s, char leastChar, int newAlphabetSize): alphabetSize(newAlphabetSize) {
     str.resize(s.length());
